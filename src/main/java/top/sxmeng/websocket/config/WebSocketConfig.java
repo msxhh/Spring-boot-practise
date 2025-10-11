584c68e678a32ad16c2c520fe65ea8492910e09c
嘿嘿哈 <449266367@qq.com>
2025-10-11 14:51:46 +0800

    添加 WebSocket 基础配置、应用启动类及全局配置

---
+++ src/main/java/top/sxmeng/websocket/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package top.sxmeng.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import top.sxmeng.websocket.handler.SimpleTimeWebSocketHandler;
+import top.sxmeng.websocket.handler.TestWebSocketHandler;
+
+/**
+ * @author alani
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 测试的 WebSocket 端点
+        registry.addHandler(new TestWebSocketHandler(), "/ws/test")
+                .setAllowedOrigins("*");
+
+        registry.addHandler(new SimpleTimeWebSocketHandler(), "/ws/time").setAllowedOrigins("*");
+    }
+}
